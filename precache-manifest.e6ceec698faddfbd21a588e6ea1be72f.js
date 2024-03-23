e2e2a69fe3ddf17499210dcc4b07ae0d5f4aff75
kunal khurana <kunalkhurana250@gmail.com>
2024-03-23 18:50:05 +0530

    Updates

---
+++ precache-manifest.e6ceec698faddfbd21a588e6ea1be72f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "36950f67de436c61eedfc850c36f7fb3",
+    "url": "/react_components-pages/index.html"
+  },
+  {
+    "revision": "c81e33bcf6a0404d2a81",
+    "url": "/react_components-pages/static/css/main.afd377d1.chunk.css"
+  },
+  {
+    "revision": "ae05e3cd37358f0c71a4",
+    "url": "/react_components-pages/static/js/2.9980a0c2.chunk.js"
+  },
+  {
+    "revision": "6c4bfe61775909bc4616",
+    "url": "/react_components-pages/static/js/3.68d536b4.chunk.js"
+  },
+  {
+    "revision": "c81e33bcf6a0404d2a81",
+    "url": "/react_components-pages/static/js/main.6350b570.chunk.js"
+  },
+  {
+    "revision": "65f7cd5776f2217af5ae",
+    "url": "/react_components-pages/static/js/runtime~main.c8f2e040.js"
+  }
+]);
